7ebf4669c5c12bd862a44431c6e2641da47100cb
Jefferson Simão Gonçalves <gerson.simao.92@gmail.com>
2018-06-08 16:53:04 -0300

    Update plugin

---
+++ src/Plugin.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jefferson Simão Gonçalves
+ * Email: gerson.simao.92@gmail.com
+ * Date: 08/06/2018
+ * Time: 16:48
+ */
+
+namespace FractalTransformerView;
+
+
+class Plugin
+{
+
+}
